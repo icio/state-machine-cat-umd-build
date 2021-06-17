abde773636aa5cd14f4cadcc75fdc501ea763677
Paul Scott <paul.scott@ravelin.com>
2021-06-17 12:35:26 +0100

    umd, minify.

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import json from '@rollup/plugin-json';
 import commonjs from '@rollup/plugin-commonjs';
 import nodePolyfills from 'rollup-plugin-node-polyfills';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
 
 export default {
   input: 'node_modules/state-machine-cat/src/index.js',
@@ -12,8 +13,9 @@ export default {
     nodeResolve(),
   ],
   output: {
-    file: 'dist/smcat.js',
+    file: 'umd/smcat.min.js',
     format: 'umd',
     name: 'smcat',
+    plugins: [terser()],
   }
 };
